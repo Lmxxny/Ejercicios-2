24db9b2106faed2b0ad4fae33aa22d34613be267
Park Jihoon <trabajostodo0@gmail.com>
2024-05-05 16:54:23 -0600

    ejercicio 11

---
+++ Coseno/Ejercicio 11/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Ejercicio_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtcos = new System.Windows.Forms.TextBox();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtcos
+            // 
+            this.txtcos.Location = new System.Drawing.Point(143, 221);
+            this.txtcos.Name = "txtcos";
+            this.txtcos.Size = new System.Drawing.Size(131, 29);
+            this.txtcos.TabIndex = 15;
+            this.txtcos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(143, 104);
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(131, 29);
+            this.txtn.TabIndex = 14;
+            this.txtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 22);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Coseno:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 22);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Ingrese el ángulo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 26);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Calcular el coseno de:";
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Location = new System.Drawing.Point(272, 157);
+            this.btncerrar.Margin = new System.Windows.Forms.Padding(5);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(102, 47);
+            this.btncerrar.TabIndex = 10;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Location = new System.Drawing.Point(155, 157);
+            this.btncalc.Margin = new System.Windows.Forms.Padding(5);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(107, 47);
+            this.btncalc.TabIndex = 9;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Location = new System.Drawing.Point(53, 157);
+            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(92, 47);
+            this.btnlimpiar.TabIndex = 8;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(433, 283);
+            this.Controls.Add(this.txtcos);
+            this.Controls.Add(this.txtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtcos;
+        private System.Windows.Forms.TextBox txtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+    }
+}
+
---
+++ Coseno/Ejercicio 11/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtcos.Clear();
+            txtn.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            double grados;
+            double radianes;
+            double cos;
+            if (string.IsNullOrEmpty(txtn.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                grados = Convert.ToDouble(txtn.Text);
+                radianes = Math.PI * grados / 180.0;
+                cos = Math.Cos(radianes);
+
+                txtcos.Text = cos.ToString();
+            }
+        }
+    }
+}
