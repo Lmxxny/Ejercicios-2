6ae9d7194356152663c8b45cdbe2dc160832e1a1
Park Jihoon <trabajostodo0@gmail.com>
2024-05-05 19:05:12 -0600

    ejercicio 12

---
+++ y, f, plg y c/Convertir/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Convertir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.txtcm = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncerrar.Location = new System.Drawing.Point(377, 257);
+            this.btncerrar.Margin = new System.Windows.Forms.Padding(5);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(155, 66);
+            this.btncerrar.TabIndex = 29;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalc.Location = new System.Drawing.Point(212, 257);
+            this.btncalc.Margin = new System.Windows.Forms.Padding(5);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(155, 66);
+            this.btncalc.TabIndex = 28;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(47, 257);
+            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(155, 66);
+            this.btnlimpiar.TabIndex = 27;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(274, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 23);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "A:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 23);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Convertir de:";
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(224, 123);
+            this.txtn.Margin = new System.Windows.Forms.Padding(5);
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(122, 29);
+            this.txtn.TabIndex = 24;
+            this.txtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtcm
+            // 
+            this.txtcm.Location = new System.Drawing.Point(269, 196);
+            this.txtcm.Margin = new System.Windows.Forms.Padding(5);
+            this.txtcm.Name = "txtcm";
+            this.txtcm.Size = new System.Drawing.Size(127, 29);
+            this.txtcm.TabIndex = 31;
+            this.txtcm.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Yardas",
+            "Pies",
+            "Pulgadas"});
+            this.comboBox1.Location = new System.Drawing.Point(224, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 30);
+            this.comboBox1.TabIndex = 34;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 22);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Centímetros:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(568, 356);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtcm);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtn);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtn;
+        private System.Windows.Forms.TextBox txtcm;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ y, f, plg y c/Convertir/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convertir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtcm.Clear();
+            txtn.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            double n;
+            double resultado;
+            resultado = 0;
+            if (string.IsNullOrEmpty(txtn.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                n = Convert.ToDouble(txtn.Text);
+                string unidad;
+                unidad = comboBox1.SelectedItem.ToString();
+
+                switch (unidad)
+                {
+                    case "Yardas":
+                        resultado = n * 91.44;
+                        break;
+                    case "Pulgadas":
+                        resultado = n * 2.54; 
+                        break;
+                    case "Pies":
+                        resultado = n * 30.48; 
+                        break;
+                }
+
+                txtcm.Text = resultado.ToString();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
