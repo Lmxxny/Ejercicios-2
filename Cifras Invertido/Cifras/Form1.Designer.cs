15fd23c2b3fc7f687a068b820a609381c17e14d3
Park Jihoon <trabajostodo0@gmail.com>
2024-05-06 18:56:03 -0600

    ejercicio 18

--- Cifras Invertido/Cifras/Form1.Designer.cs
+++ Cifras Invertido/Cifras/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Numero
+﻿namespace Cifras
 {
     partial class Form1
     {
@@ -28,23 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
             this.btncerrar = new System.Windows.Forms.Button();
             this.btncalc = new System.Windows.Forms.Button();
             this.btnlimpiar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtn = new System.Windows.Forms.TextBox();
-            this.txtpoi = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.txtcifra = new System.Windows.Forms.TextBox();
+            this.txtinvertido = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 22);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "El número es:";
             // 
             // btncerrar
             // 
             this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
             this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btncerrar.Location = new System.Drawing.Point(329, 178);
+            this.btncerrar.Location = new System.Drawing.Point(321, 195);
             this.btncerrar.Name = "btncerrar";
             this.btncerrar.Size = new System.Drawing.Size(138, 56);
-            this.btncerrar.TabIndex = 17;
+            this.btncerrar.TabIndex = 31;
             this.btncerrar.Text = "Cerrar";
             this.btncerrar.UseVisualStyleBackColor = false;
             this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
@@ -53,10 +62,10 @@
             // 
             this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
             this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btncalc.Location = new System.Drawing.Point(185, 178);
+            this.btncalc.Location = new System.Drawing.Point(177, 195);
             this.btncalc.Name = "btncalc";
             this.btncalc.Size = new System.Drawing.Size(138, 56);
-            this.btncalc.TabIndex = 16;
+            this.btncalc.TabIndex = 30;
             this.btncalc.Text = "Calcular";
             this.btncalc.UseVisualStyleBackColor = false;
             this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
@@ -65,10 +74,10 @@
             // 
             this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
             this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnlimpiar.Location = new System.Drawing.Point(41, 178);
+            this.btnlimpiar.Location = new System.Drawing.Point(33, 195);
             this.btnlimpiar.Name = "btnlimpiar";
             this.btnlimpiar.Size = new System.Drawing.Size(138, 56);
-            this.btnlimpiar.TabIndex = 15;
+            this.btnlimpiar.TabIndex = 29;
             this.btnlimpiar.Text = "Limpiar";
             this.btnlimpiar.UseVisualStyleBackColor = false;
             this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
@@ -77,50 +86,43 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(150, 23);
+            this.label1.Location = new System.Drawing.Point(100, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 23);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Escriba un número:";
+            this.label1.Size = new System.Drawing.Size(276, 23);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Provea un número de 3 cifras:";
             // 
-            // txtn
+            // txtcifra
             // 
-            this.txtn.Location = new System.Drawing.Point(173, 59);
-            this.txtn.Name = "txtn";
-            this.txtn.Size = new System.Drawing.Size(137, 29);
-            this.txtn.TabIndex = 12;
+            this.txtcifra.Location = new System.Drawing.Point(165, 76);
+            this.txtcifra.Name = "txtcifra";
+            this.txtcifra.Size = new System.Drawing.Size(137, 29);
+            this.txtcifra.TabIndex = 27;
+            this.txtcifra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // txtpoi
+            // txtinvertido
             // 
-            this.txtpoi.Location = new System.Drawing.Point(236, 114);
-            this.txtpoi.Name = "txtpoi";
-            this.txtpoi.Size = new System.Drawing.Size(143, 29);
-            this.txtpoi.TabIndex = 11;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(112, 117);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 23);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "El número es:";
+            this.txtinvertido.Location = new System.Drawing.Point(228, 131);
+            this.txtinvertido.Name = "txtinvertido";
+            this.txtinvertido.Size = new System.Drawing.Size(143, 29);
+            this.txtinvertido.TabIndex = 26;
+            this.txtinvertido.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(507, 253);
+            this.ClientSize = new System.Drawing.Size(502, 283);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btncerrar);
             this.Controls.Add(this.btncalc);
             this.Controls.Add(this.btnlimpiar);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtn);
-            this.Controls.Add(this.txtpoi);
+            this.Controls.Add(this.txtcifra);
+            this.Controls.Add(this.txtinvertido);
             this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -130,13 +132,13 @@
 
         #endregion
 
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btncerrar;
         private System.Windows.Forms.Button btncalc;
         private System.Windows.Forms.Button btnlimpiar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtn;
-        private System.Windows.Forms.TextBox txtpoi;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcifra;
+        private System.Windows.Forms.TextBox txtinvertido;
     }
 }
 
---
+++ Cifras Invertido/Cifras/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cifras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtcifra.Clear();
+            txtinvertido.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            int cifra;
+            int invertido;
+
+            if (string.IsNullOrEmpty(txtcifra.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                cifra = Convert.ToInt32(txtcifra.Text);
+
+                if ( cifra <= 99 )
+                {
+                    MessageBox.Show("Por favor, ingresa un valor de tres cifras", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }else if (cifra >= 1000)
+                {
+                    MessageBox.Show("Por favor, ingresa un valor de tres cifras", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else {
+                    int centenas = cifra / 100;
+                    int decenas = (cifra % 100) / 10;
+                    int unidades = cifra % 10;
+
+                    invertido = (unidades * 100) + (decenas * 10) + centenas;
+
+                    txtinvertido.Text = invertido.ToString();
+                }
+                }
+            }
+        }
+    }
