bc498c00c7a85f66a3440cd9ddde77ea2ff20991
Park Jihoon <trabajostodo0@gmail.com>
2024-05-07 19:26:23 -0600

    ejercicio 16

---
+++ matriz/matrix/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace matrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtx = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txty = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtf = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txte = new System.Windows.Forms.TextBox();
+            this.txtd = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(466, 191);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(98, 29);
+            this.txtc.TabIndex = 91;
+            this.txtc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 23);
+            this.label7.TabIndex = 90;
+            this.label7.Text = "c=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(288, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 23);
+            this.label5.TabIndex = 89;
+            this.label5.Text = "b=";
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(325, 191);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(98, 29);
+            this.txtb.TabIndex = 88;
+            this.txtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtx
+            // 
+            this.txtx.Location = new System.Drawing.Point(323, 386);
+            this.txtx.Name = "txtx";
+            this.txtx.Size = new System.Drawing.Size(104, 29);
+            this.txtx.TabIndex = 87;
+            this.txtx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 389);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 23);
+            this.label3.TabIndex = 86;
+            this.label3.Text = "x=";
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncerrar.Location = new System.Drawing.Point(433, 320);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(103, 44);
+            this.btncerrar.TabIndex = 85;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalc.Location = new System.Drawing.Point(324, 320);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(103, 44);
+            this.btncalc.TabIndex = 84;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(216, 320);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(103, 44);
+            this.btnlimpiar.TabIndex = 83;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(184, 191);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(98, 29);
+            this.txta.TabIndex = 82;
+            this.txta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 23);
+            this.label2.TabIndex = 81;
+            this.label2.Text = "a=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(655, 26);
+            this.label1.TabIndex = 80;
+            this.label1.Text = "Determinar las solucionespara un sistema de ecuaciones lineales:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(343, 23);
+            this.label4.TabIndex = 94;
+            this.label4.Text = "Ingrese los coeficientes para la ecuación:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(312, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 23);
+            this.label8.TabIndex = 95;
+            this.label8.Text = "ax + by = c";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(314, 145);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 23);
+            this.label9.TabIndex = 96;
+            this.label9.Text = "dx + ey = f";
+            // 
+            // txty
+            // 
+            this.txty.Location = new System.Drawing.Point(323, 434);
+            this.txty.Name = "txty";
+            this.txty.Size = new System.Drawing.Size(104, 29);
+            this.txty.TabIndex = 98;
+            this.txty.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(288, 437);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 23);
+            this.label10.TabIndex = 97;
+            this.label10.Text = "y=";
+            // 
+            // txtf
+            // 
+            this.txtf.Location = new System.Drawing.Point(466, 256);
+            this.txtf.Name = "txtf";
+            this.txtf.Size = new System.Drawing.Size(98, 29);
+            this.txtf.TabIndex = 104;
+            this.txtf.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(432, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 23);
+            this.label6.TabIndex = 103;
+            this.label6.Text = "f=";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(288, 259);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 23);
+            this.label11.TabIndex = 102;
+            this.label11.Text = "e=";
+            // 
+            // txte
+            // 
+            this.txte.Location = new System.Drawing.Point(325, 256);
+            this.txte.Name = "txte";
+            this.txte.Size = new System.Drawing.Size(98, 29);
+            this.txte.TabIndex = 101;
+            this.txte.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtd
+            // 
+            this.txtd.Location = new System.Drawing.Point(184, 256);
+            this.txtd.Name = "txtd";
+            this.txtd.Size = new System.Drawing.Size(98, 29);
+            this.txtd.TabIndex = 100;
+            this.txtd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(149, 259);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(31, 23);
+            this.label12.TabIndex = 99;
+            this.label12.Text = "d=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(743, 494);
+            this.Controls.Add(this.txtf);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txte);
+            this.Controls.Add(this.txtd);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txty);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txtx);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txty;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtf;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txte;
+        private System.Windows.Forms.TextBox txtd;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ matriz/matrix/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txta.Clear();
+            txtb.Clear();
+            txtc.Clear();
+            txtd.Clear();
+            txte.Clear();
+            txtf.Clear();
+            txtx.Clear();
+            txty.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            double a, b, c, d, ee, f;
+
+            if (string.IsNullOrWhiteSpace(txta.Text) || string.IsNullOrEmpty(txtb.Text) || string.IsNullOrEmpty(txtc.Text) || string.IsNullOrEmpty(txtd.Text) || string.IsNullOrEmpty(txte.Text) || string.IsNullOrEmpty(txtf.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                a = Convert.ToDouble(txta.Text);
+                b = Convert.ToDouble(txtb.Text);
+                c = Convert.ToDouble(txtc.Text);
+                d = Convert.ToDouble(txtd.Text);
+                ee = Convert.ToDouble(txte.Text);
+                f = Convert.ToDouble(txtf.Text);
+
+                double det;
+                double detX;
+                double detY;
+
+                det = a * ee - b * d;
+                detX = c * ee - b * f;
+                detY = a * f - c * d;
+
+                if (det != 0)
+                {
+                    // Calcular las soluciones
+                    double x = detX / det;
+                    double y = detY / det;
+                    txtx.Text = x.ToString();
+                    txty.Text = y.ToString();
+                }
+                else
+                {
+                    if (detX == 0 && detY == 0)
+                    {
+                        MessageBox.Show("El sistema tiene infinitas soluciones.", " ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("El sistema no tiene solución.", " ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+        }
+    }
+}
